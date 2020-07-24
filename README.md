b051feec2634b9aef7dbc987946dc042d4a4a61e
Tomáš Kebrle <56836089+Kendy205@users.noreply.github.com>
2020-07-24 13:48:13 +0200

    Kompletní automtizace
    Woohooo, celý to funguje a je to kompletně automatizovaný

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Python script na vytváření poznávačky
+
+#Jak na používání?
+
+Pro použití si potřebujete stáhnout programovací jazyk Python (https://python.org), a doporučuju nějaký IDE, nejlepšíí bude asi PyCharm od JetBrains, nebo Visual Studio Code od Microsoftu, v IDE pak klikněte na tlačítko **RUN** a terminal po vás bude chtít URL, prosím zadávejte jen URL který končí na .jpg jinak to nebude fungovat, po zadání URL zadejte název kytky, stromu atd.  pro další opakujte tento proces
--- main.py
+++ main.py
@@ -1,16 +1,63 @@
+#Script pro vytváření poznáváček
+#By: Tomáš Kebrle
+
+#! Knihovna pro stahování
 import urllib.request
 
-#Stáhnutí obrázku
+
+#Proces pro stahování obrázku
 def dl_jpg(url, file_path, file_name):
     full_path = file_path + file_name + '.jpg'
     urllib.request.urlretrieve(url, full_path)
 
+#Vytvoření HTML souboru
+def html_create(file_name, url, nazev, btn1, btn2):
+    f = open(file_name + '.html', 'w')
+    f.write('<!DOCTYPE html>'
+            '<html lang="en">'
+            '<head>'
+            '<meta charset="UTF-8">'
+            '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
+            '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">'
+            '<style>'
+            'img{height:50% !important;width:50% !important;overflow: hidden;}'
+            '</style>'
+            '<title>' + nazev + '</title>'
+            '</head>'
+            '<body>'
+            '<div class="container">'
+            '<div class="row">'
+            '<div class="img">'
+            '<img class="col s12 l8 center" src="images/' + file_name + '.jpg">'
+            '</div>'
+            '<div class="center col s12 l4">'
+            '<h1 class="center">' + nazev + '</h1> <br> <br> <br>'
+            + '<p class="grey-text lighten-2 col s12 l12 center">' + url + '</p>'
+            '<a href="' + btn1 + '.html" class="btn blue waves-effect waves-light">Předchozí obrázek</a>'
+            '<a href="' + btn2 + '.html" class="btn blue waves-effect waves-light">Následující obrázek</a>' 
+            '</div>'
+            '</div>'
+            '</div>'
+            '</body>'
+            '</html>'
+            )
+
+#!Důležité pro loopování
 file_name = 0
+
 #Loop pro stahování a pojmenování obrázku
 while 2 < 3:
-    url = input('URL:')
+    url = input('Zadejte URL obrázku:')
+    nazev = input('Název:')
     file_name = int(file_name)
     file_name += 1
+    tlacitko1 = file_name - 1
+    tlacitko2 = file_name + 1
     file_name = str(file_name)
-
+    tlacitko1 = str(tlacitko1)
+    tlacitko2 = str(tlacitko2)
+    #Download proces
     dl_jpg(url, 'images/', file_name)
+    #vytvořit HTML file
+    html_create(file_name, url, nazev, tlacitko1, tlacitko2)
+
