280c7ea95aa6b970c147fbd60787f4916341f5bc
Tomáš Kebrle <56836089+Kendy205@users.noreply.github.com>
2020-12-17 20:14:33 +0100

    Verze fucking 2.0

--- main.py
+++ main.py
@@ -1,18 +1,59 @@
 #Script pro vytváření poznáváček
 #By: Tomáš Kebrle
 
-#! Knihovna pro stahování
+# Knihovny
 import urllib.request
+from urllib.request import urlopen
 from tkinter import *
+import wikipediaapi
+from bs4 import BeautifulSoup
+import lxml
 
 
-#Proces pro stahování obrázku
+#Wiki integrace
+wiki = wikipediaapi.Wikipedia("cs")
+
+file_name = 0
+def click():
+        #Tlačítka a název
+        global  file_name
+        click_nazev = nazev.get()
+        file_name = int(file_name)
+        file_name += 1
+        tlacitko1 = file_name - 1
+        tlacitko2 = file_name + 1
+        file_name = str(file_name)
+        tlacitko1 = str(tlacitko1)
+        tlacitko2 = str(tlacitko2)
+        #Scrapnutí
+        wiki_page = wiki.page(click_nazev)
+        wiki_page_en = wiki_page.langlinks['en']
+        wiki_url = wiki_page_en.fullurl
+        html = urlopen(wiki_url)
+        bs = BeautifulSoup(html, 'html.parser')
+        table = bs.find('a' ,attrs={"class":"image"})
+        table_f = open('table.html', 'w')
+        table_f.write(str(table))
+        with open('table.html', 'r') as table_f:
+            table_contents = table_f.read()
+            more_bs = BeautifulSoup(table_contents, 'lxml')
+            img = more_bs.find('img')
+            image = img['src']
+            wiki_image = 'https:' + image
+            #Download proces
+            dl_jpg(wiki_image, 'images/', file_name)
+            #vytvořit HTML file
+            html_create(file_name, click_nazev, tlacitko1, tlacitko2)
+
+#Download
 def dl_jpg(url, file_path, file_name):
     full_path = file_path + file_name + '.jpg'
     urllib.request.urlretrieve(url, full_path)
 
+
+
 #Vytvoření HTML souboru
-def html_create(file_name, url, nazev, btn1, btn2):
+def html_create(file_name, nazev, btn1, btn2):
     f = open(file_name + '.html', 'w')
     f.write('<!DOCTYPE html>'
             '<html lang="en">'
@@ -33,9 +74,8 @@ def html_create(file_name, url, nazev, btn1, btn2):
             '</div>'
             '<div class="center col s12 l4">'
             '<h1 class="center">' + nazev + '</h1> <br> <br> <br>'
-            + '<p class="grey-text lighten-2 col s12 l12 center">' + url + '</p>'
             '<a href="' + btn1 + '.html" class="btn blue waves-effect waves-light">Previous</a>'
-            '<a href="' + btn2 + '.html" class="btn blue waves-effect waves-light">Next</a>' 
+            '<a href="' + btn2 + '.html" class="btn blue waves-effect waves-light">Next</a>'
             '</div>'
             '</div>'
             '</div>'
@@ -43,64 +83,19 @@ def html_create(file_name, url, nazev, btn1, btn2):
             '</html>'
             )
 
-file_name = 0
-def click():
-        global  file_name
-        click_url =  url.get()
-        click_nazev = nazev.get()
-        file_name = int(file_name)
-        file_name += 1
-        tlacitko1 = file_name - 1
-        tlacitko2 = file_name + 1
-        file_name = str(file_name)
-        tlacitko1 = str(tlacitko1)
-        tlacitko2 = str(tlacitko2)
-        #Download proces
-        dl_jpg(click_url, 'images/', file_name)
-        #vytvořit HTML file
-        html_create(file_name, click_url, click_nazev, tlacitko1, tlacitko2)
-
-####GUI
+#Tkinter
 window = Tk()
 window.title('Poznávačka Maker 3000')
 
 # Label
 Label(window, text='POZNÁVAČKA MAKER 3000', font='none 32 bold').grid(row=0, column=0, sticky=W, padx=20)
 
-# Text Entry
-Label(window, text='URL obrázku(musí končit na .jpg)', font='none 11').grid(row=3, column=0, sticky=W, padx=20)
-url = Entry(window, width=80, )
-url.grid(row=5, column=0, sticky=W, padx=20)
-
+#Název kytky
 Label(window, text='Název kytky rostliny atd.', font='none 11').grid(row=7, column=0, sticky=W, padx=20)
 nazev = Entry(window, width=80, )
 nazev.grid(row=9, column=0, sticky=W, padx=20)
 
 # Submit
 Button(window, text='POTVRDIT', width='6', command=click).grid(row=11, column=0, sticky=W, padx=20)
-
-
-
-
-
-
-
-
-
-#Loop pro stahování a pojmenování obrázku
-#while 2 < 3:
-#   url = input('Zadejte URL obrázku:')
-#   nazev = input('Název:')
-#   file_name = int(file_name)
-#   file_name += 1
-#   tlacitko1 = file_name - 1
-#    tlacitko2 = file_name + 1
-#    file_name = str(file_name)
-#    tlacitko1 = str(tlacitko1)
-#    tlacitko2 = str(tlacitko2)
-#    #Download proces
-#    dl_jpg(url, 'images/', file_name)
-#    #vytvořit HTML file
-#    html_create(file_name, url, nazev, tlacitko1, tlacitko2)
 
 window.mainloop()
