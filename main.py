7728a56c6c8cf1c9e673c2d6a58a4b5ffe70119f
Tomáš Kebrle <56836089+Kendy205@users.noreply.github.com>
2020-12-23 14:29:16 +0100

    Update kvuli tomu ze windows je sracka

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#Script pro vytváření poznáváček
-#By: Tomáš Kebrle
+# Script pro vytváření poznáváček
+# By: Tomáš Kebrle
 
 # Knihovny
 import urllib.request
@@ -10,46 +10,49 @@ from bs4 import BeautifulSoup
 import lxml
 from pptx import Presentation
 
-#Important stuff
+# Important stuff
 wiki = wikipediaapi.Wikipedia("cs")
 
 file_name = 0
-def click():
-        #Tlačítka a název
-        global  file_name
-        click_nazev = nazev.get()
-        file_name = int(file_name)
-        file_name += 1
-        tlacitko1 = file_name - 1
-        tlacitko2 = file_name + 1
-        file_name = str(file_name)
-        tlacitko1 = str(tlacitko1)
-        tlacitko2 = str(tlacitko2)
-        #Scrapnutí
-        wiki_page = wiki.page(click_nazev)
-        wiki_page_en = wiki_page.langlinks['en']
-        wiki_url = wiki_page_en.fullurl
-        html = urlopen(wiki_url)
-        bs = BeautifulSoup(html, 'html.parser')
-        a_link = bs.find('a' ,attrs={"class":"image"})
-        img = a_link.find('img')
-        image = img['src']
-        wiki_image = 'https:' + image
-        #Download proces
-        dl_jpg(wiki_image, 'images/', file_name)
-        #vytvořit HTML file
-        html_create(file_name, click_nazev, tlacitko1, tlacitko2)
 
-#Download
+
+def click():
+    # Tlačítka a název
+    global file_name
+    click_nazev = nazev.get()
+    file_name = int(file_name)
+    file_name += 1
+    tlacitko1 = file_name - 1
+    tlacitko2 = file_name + 1
+    file_name = str(file_name)
+    tlacitko1 = str(tlacitko1)
+    tlacitko2 = str(tlacitko2)
+    # Scrapnutí
+    wiki_page = wiki.page(click_nazev)
+    wiki_page_en = wiki_page.langlinks['en']
+    wiki_url = wiki_page_en.fullurl
+    html = urlopen(wiki_url)
+    bs = BeautifulSoup(html, 'html.parser')
+    a_link = bs.find('a', attrs={"class": "image"})
+    img = a_link.find('img')
+    image = img['src']
+    wiki_image = 'https:' + image
+    # Download proces
+    dl_jpg(wiki_image, 'images/', file_name)
+    # vytvořit HTML file
+    html_create(file_name, click_nazev, tlacitko1, tlacitko2)
+
+# Download
+
+
 def dl_jpg(url, file_path, file_name):
     full_path = file_path + file_name + '.jpg'
     urllib.request.urlretrieve(url, full_path)
 
 
-
-#Vytvoření HTML souboru
+# Vytvoření HTML souboru
 def html_create(file_name, nazev, btn1, btn2):
-    f = open(file_name + '.html', 'w')
+    f = open(file_name + '.html', 'w', encoding='utf-8')
     f.write('<!DOCTYPE html>'
             '<html lang="en">'
             '<head>'
@@ -75,22 +78,25 @@ def html_create(file_name, nazev, btn1, btn2):
             '</div>'
             '</div>'
             '</body>'
-            '</html>'
-            )
+            '</html>')
 
-#Tkinter
+
+# Tkinter
 window = Tk()
 window.title('Poznávačka Maker 3000')
 
 # Label
-Label(window, text='POZNÁVAČKA MAKER 3000', font='none 32 bold').grid(row=0, column=0, sticky=W, padx=20)
+Label(window, text='POZNÁVAČKA MAKER 3000', font='none 32 bold').grid(
+    row=0, column=0, sticky=W, padx=20)
 
-#Název kytky
-Label(window, text='Název kytky rostliny atd.', font='none 11').grid(row=7, column=0, sticky=W, padx=20)
+# Název kytky
+Label(window, text='Název kytky rostliny atd.', font='none 11').grid(
+    row=7, column=0, sticky=W, padx=20)
 nazev = Entry(window, width=80, )
 nazev.grid(row=9, column=0, sticky=W, padx=20)
 
 # Submit
-Button(window, text='POTVRDIT', width='6', command=click).grid(row=11, column=0, sticky=W, padx=20)
+Button(window, text='POTVRDIT', width='6', command=click).grid(
+    row=11, column=0, sticky=W, padx=20)
 
 window.mainloop()
